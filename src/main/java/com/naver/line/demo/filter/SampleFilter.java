23fa93d3a1dd6386399cfd664d4ebf3c8ccb2a28
lshn1007 <lshn1007@hanyang.ac.kr>
2022-05-21 23:25:12 +0900

    feat: Filter 에러메시지 커스토마이징

--- src/main/java/com/naver/line/demo/filter/SampleFilter.java
+++ src/main/java/com/naver/line/demo/filter/SampleFilter.java
@@ -1,7 +1,11 @@
 package com.naver.line.demo.filter;
 
+import com.naver.line.demo.utils.ApiUtils;
+import org.springframework.http.HttpStatus;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class SampleFilter implements Filter {
@@ -9,10 +13,21 @@ public class SampleFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String userId = request.getHeader("X-USER-ID");
-        if (userId == null) {
-            throw new ServletException ("Unauthorized access, unable to forward to login page");
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        try {
+            String userId = request.getHeader("X-USER-ID");
+            if (userId == null) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                String errorMsg = ApiUtils.error("hello error message", HttpStatus.UNAUTHORIZED).toString();
+                response.getWriter().write(errorMsg);
+                return;
+            }
+            filterChain.doFilter(servletRequest, servletResponse);
+        } catch (Exception e) {
+            throw e;
         }
-        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
